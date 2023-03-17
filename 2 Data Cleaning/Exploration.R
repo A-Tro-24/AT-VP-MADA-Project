b6ee79739e096417fd152ea6b5e31362c6e7fa62
Aidan Troha <aht24204@uga.edu>
2023-03-17 17:18:11 -0400

    Moving the Manuscript
    information moved to README file

--- 2 Data Cleaning/Exploration.R
+++ 2 Data Cleaning/Exploration.R
@@ -11,9 +11,9 @@ plot <- ggplot(dat)
 plot + geom_histogram(aes(x=age), bins = 45)
 # Plots a histogram showing the distribution of COVID deaths by per capita income
 plot + geom_histogram(aes(x=per_capita_income/1000), bins = 45) +
-       labs(xlab = "per Capita Income (in thousands of $)",
-            ylab = "# of Deaths",
-            title = "Distribution of COVID Deaths in Georgia by per Capita Income")
+       labs(title = "Distribution of COVID Deaths in Georgia by per Capita Income",
+            xlab = "per Capita Income (in thousands of $)",
+            ylab = "# of Deaths")
 # Plots the number of COVID deaths by sex
 plot + geom_bar(aes(x=sex))
 # Plots the number of COVID deaths by majority party
@@ -21,3 +21,6 @@ plot + geom_bar(aes(x=party_majority))
 # Plots the number of COVID deaths by whether the subject was known to
 # have had a pre-existing chronic condition
 plot + geom_bar(aes(x=chronic_condition))
+# Plots the number of COVID deaths by race
+plot + geom_bar(aes(x=race, fill=ethnicity),position="stack") +
+       theme(axis.text.x = element_text(angle = 90))
